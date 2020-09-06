d186a7c83885bc7be945bb62db9228c573a3a960
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-09-06 22:04:57 +0600

    Create chat screen UI with dummy data

--- lib/screens/chat.dart
+++ lib/screens/chat.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:firebase_auth/firebase_auth.dart';
 
+import '../components/message.dart';
+
 class ChatScreen extends StatefulWidget {
   final UserCredential _user;
 
@@ -12,11 +14,94 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  var dummyData = [
+    {
+      'text': 'Hello Fahim',
+      'from': 'Fahim',
+      'me': true,
+    },
+    {
+      'text': 'Fahim',
+      'from': 'Rezuanul Fahim',
+      'me': false,
+    },
+    {
+      'text': 'Istiaq',
+      'from': 'Rezuanul',
+      'me': false,
+    },
+    {
+      'text': 'Hello Rezuan',
+      'from': 'Niloy',
+      'me': false,
+    },
+    {
+      'text': 'Rezuanul Islam Fahim',
+      'from': 'Fardous',
+      'me': false,
+    },
+    {
+      'text': 'Hello Niloy',
+      'from': 'Fahim',
+      'me': true,
+    },
+    {
+      'text': 'Hello Ashik',
+      'from': 'Steve',
+      'me': false,
+    },
+    {
+      'text': 'Hello Ashik',
+      'from': 'Steve',
+      'me': true,
+    },
+    {
+      'text': 'Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik ',
+      'from': 'Steve',
+      'me': false,
+    },
+    {
+      'text': 'Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik Hello Ashik',
+      'from': 'Steve',
+      'me': true,
+    },
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(widget._user.user.email),
+      appBar: AppBar(
+        leading: Padding(
+          padding: const EdgeInsets.all(5),
+          child: Image.asset('assets/images/logo.png'),
+        ),
+        title: Text('Chats'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemCount: dummyData.length,
+              itemBuilder: (context, index) {
+                return Message(
+                  dummyData[index]['text'],
+                  dummyData[index]['from'],
+                  dummyData[index]['me'],
+                );
+              },
+            ),
+          ),
+          Container(
+            child: Row(
+              children: <Widget>[
+                Expanded(child: TextField()),
+                IconButton(
+                  icon: Icon(Icons.send_outlined),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
