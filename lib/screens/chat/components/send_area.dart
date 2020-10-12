d48a9406c3f5468a031958a43b828aef0e4bd2b9
Rezuanul Islam Fahim <rifahim98@gmail.com>
2020-10-12 13:23:13 +0600

    Change send-area design

--- lib/screens/chat/components/send_area.dart
+++ lib/screens/chat/components/send_area.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
-import 'package:anonymous_chat/screens/chat/components/send_button.dart';
 import 'package:anonymous_chat/screens/chat/components/message_input_field.dart';
 
 // Chat screen's widget for building send-area
@@ -35,12 +34,31 @@ class SendArea extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(15),
+      padding: const EdgeInsets.fromLTRB(5, 15, 10, 15),
       child: Row(
         children: <Widget>[
-          SendButton(Icons.photo, () {}),
+          IconButton(
+            color: Theme.of(context).primaryColor,
+            icon: Icon(Icons.photo),
+            iconSize: 28,
+            splashRadius: 20,
+            onPressed: () {},
+          ),
           MessageInputField(_messageController, _sendMessage),
-          SendButton(Icons.send, _sendMessage),
+          Material(
+            clipBehavior: Clip.hardEdge,
+            borderRadius: BorderRadius.circular(30),
+            elevation: 5,
+            color: Theme.of(context).primaryColor,
+            type: MaterialType.button,
+            child: IconButton(
+              icon: Icon(Icons.send),
+              iconSize: 25,
+              color: Colors.white,
+              padding: EdgeInsets.all(12),
+              onPressed: _sendMessage,
+            ),
+          ),
         ],
       ),
     );
